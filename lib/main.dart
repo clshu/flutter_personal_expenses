d55fcc8584b56c26e40c3be205b631a412ec7dbd
clshu <clshu2020@gmail.com>
2022-01-07 20:04:56 +0800

    Checking the DEvice Platform.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 
@@ -177,8 +179,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     'Show Chart',
                     style: Theme.of(context).textTheme.headline6,
                   ),
-                  Switch(
-                      activeColor: Theme.of(context).colorScheme.primary,
+                  Switch.adaptive(
+                      // activeColor: Theme.of(context).colorScheme.secondary,
                       value: _showChart,
                       onChanged: (val) {
                         setState(() {
@@ -195,10 +197,12 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => _startAddNewTransaction(context),
-      ),
+      floatingActionButton: Platform.isIOS
+          ? Container()
+          : FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => _startAddNewTransaction(context),
+            ),
     );
   }
 }
